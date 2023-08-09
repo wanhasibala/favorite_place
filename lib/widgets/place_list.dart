bad879cc1726ab782fa327662f2cd0d310b7c3d0
wanhasibala <wanhasibal1122@gmail.com>
2023-08-09 20:36:14 +0700

    first commit

---
+++ lib/widgets/place_list.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ListPlace extends StatelessWidget {
+  const ListPlace({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Favorite Place'),
+        
+      ),
+    );
+  }
+}
